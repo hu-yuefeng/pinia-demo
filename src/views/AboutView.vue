067a007bd1fb57cddbec3e75c7e8b3b49261a674
胡跃峰 <huyuefeng@huyuefengdeMacBook-Pro.local>
2023-09-20 15:00:48 +0800

    feat:首次提交

---
+++ src/views/AboutView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <h1>count:{{ countStore.count }}</h1>
+    <button @click="countStore.increment()">+1</button>
+  </div>
+</template>
+<script setup>
+import { useCounterStore } from "../stores/counter";
+const countStore = useCounterStore();
+</script>
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
